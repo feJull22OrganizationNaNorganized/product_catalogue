77a03f8aa4b0ca239aca5d691c162e103badfd5a
Cyril Prue <kir8mir>
2022-10-31 13:53:29 +0200

    ascces to data added

--- server/server.ts
+++ server/server.ts
@@ -2,13 +2,13 @@
 
 import express from 'express';
 import path from 'path';
-// import phonesFromServer from '../data/api/phones.json'
 import { readFile } from 'fs/promises';
 
 const app = express();
 const port = 8080;
 
-app.use(express.static(path.resolve('public')));
+app.use(express.static('data'))
+
 app.use(express.json());
 
 const phones = JSON.parse(
