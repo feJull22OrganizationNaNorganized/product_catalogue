819614745ac4f7b8cb7d5fb926489fc823611789
Roma Artiukh <romanskarb@ukr.net>
2022-10-31 20:08:09 +0300

    Page grid App was fixed

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 // import { PhoneCard } from './Components/PhoneCard/PhoneCard';
-import { PhonesCatalog } from './Components/PhonesCatalog/PhonesCatalog';
+// import { PhonesCatalog } from './Components/PhonesCatalog/PhonesCatalog';
 
 function App() {
   return (
     <p>Hello </p>
-    // <PhonesCatalog/>
+    
   );
 }
 
