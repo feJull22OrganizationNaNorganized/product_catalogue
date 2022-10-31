b520cbe26d49a0ef69c3ee7995e533f429955254
Roma Artiukh <romanskarb@ukr.net>
2022-10-31 20:02:42 +0300

    Page grid and catalog layot were added

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import { PhoneCard } from './Components/PhoneCard/PhoneCard';
+// import { PhoneCard } from './Components/PhoneCard/PhoneCard';
+import { PhonesCatalog } from './Components/PhonesCatalog/PhonesCatalog';
 
 function App() {
   return (
     <p>Hello </p>
+    // <PhonesCatalog/>
   );
 }
 
