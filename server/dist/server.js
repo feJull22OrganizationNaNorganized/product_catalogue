7b82c7d1c751fe863cac2b272afa075eb672f3c6
Cyril Prue <kir8mir>
2022-11-02 14:57:02 +0200

    swc installed, broject builded

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,29 +1,17 @@
 'use strict';
-
 import express from 'express';
-import path from 'path';
 // import phonesFromServer from '../data/api/phones.json'
 import { readFile } from 'fs/promises';
 import cors from 'cors';
-
 const app = express();
 const port = process.env.PORT || 8080;
-
 app.use(cors());
-app.use(express.static('data'))
+app.use(express.static('data'));
 app.use(express.json());
-
-
-const phones = JSON.parse(
-  await readFile(
-    new URL('data/api/phones.json', import.meta.url),
-    'utf-8'
-));
-
+const phones = JSON.parse(await readFile(new URL('data/api/phones.json', import.meta.url), 'utf-8'));
 app.get('/phones', (req, res) => {
-  console.log('Server is running');
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.send(phones);
+    console.log('Server is running');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(phones);
 });
-
 app.listen(port);
