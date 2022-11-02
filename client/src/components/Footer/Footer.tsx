70503e88c43985e7561f79da2eabfd8a3dea15bf
Oksana Kyryienko <kseniyals1609@gmail.com>
2022-11-02 14:46:41 +0200

    some fixes for header footer fonts were added

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
 import './Footer.scss';
+import '../../styles/index.scss';
 
 
 export function Footer () {
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,55 +1,58 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
 import './Header.scss';
 
 export function Header() {
   return (
-    <div className="header">
+    <div className="header" id="header">
       <div className="header__container">
-        <div className="images">
-          <div className="nice_gadgets">
-            <img
-              className="nice"
-              src={require('../../images/Nice_Gadgets.png')}
-              alt="footerLogo"
-            />
+        <div className="menu__container">
+          <div className="images">
+            <div className="nice_gadgets">
+              <img
+                className="nice"
+                src={require('../../images/Nice_Gadgets.png')}
+                alt="footerLogo"
+              />
+            </div>
+            <div className="img_hand">
+              <img
+                className="hand"
+                src={require('../../images/imageHand.png')}
+                alt="hand_ok"
+              />
+            </div>
           </div>
-          <div className="img_hand">
-            <img
-              className="hand"
-              src={require('../../images/imageHand.png')}
-              alt="hand_ok"
-            />
-          </div>
-        </div>
-        <nav className="header__menu">
-          <div className="active">
+          <nav className="header__menu">
+            <div className="active">
+              <ul className="nav__list">
+                <li className="nav__item">
+                  <a className="nav__link is-active" href="#section-1" >Home
+                  </a>
+                </li>
+              </ul>
+              <div className='line'></div>
+            </div>
             <ul className="nav__list">
               <li className="nav__item">
-                <a className="nav__link is-active" href="#section-1" >Home
+                <a className="nav__link" href="#section-2" >Phones
                 </a>
               </li>
             </ul>
-            <div className='line'></div>
-          </div>
-          <ul className="nav__list">
-            <li className="nav__item">
-              <a className="nav__link" href="#section-2" >Phones
-              </a>
-            </li>
-          </ul>
-          <ul className="nav__list">
-            <li className="nav__item">
-              <a className="nav__link" href="#section-3" >Tablets
-              </a>
-            </li>
-          </ul>
-          <ul className="nav__list">
-            <li className="nav__item">
-              <a className="nav__link" href="#section-4" >Accessories
-              </a>
-            </li>
-          </ul>
-        </nav>
+            <ul className="nav__list">
+              <li className="nav__item">
+                <a className="nav__link" href="#section-3" >Tablets
+                </a>
+              </li>
+            </ul>
+            <ul className="nav__list">
+              <li className="nav__item">
+                <a className="nav__link" href="#section-4" >Accessories
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <div className="header__top">
           <div className="top_img">
             <a href="#header">
@@ -75,8 +78,8 @@ export function Header() {
             <div className="container_img">
               <img
                 className="cart"
-                src={require('../../images/Cart.svg')}
-                alt="cart"
+                src={require('../../images/cart.png')}
+                alt="Сart"
               />
             </div>
           </a>
