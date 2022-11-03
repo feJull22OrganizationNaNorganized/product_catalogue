7f7605f4d5afd44f6200bcd3d9337e423b95c616
YuriiHoliuk <yurii.holiuk@mate.academy>
2022-11-03 11:47:52 +0200

    Working server and cors

---
+++ server/dist/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+// import phonesFromServer from '../data/api/phones.json'
+import { readFile } from 'fs/promises';
+import cors from 'cors';
+const app = express();
+const port = 8080;
+app.use(cors());
+app.use(express.static('data'));
+app.use(express.json());
+const phones = JSON.parse(await readFile(new URL('../data/api/phones.json', import.meta.url), 'utf-8'));
+app.get('/phones', (req, res) => {
+    console.log('Server is running');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(phones);
+});
+app.listen(port);
