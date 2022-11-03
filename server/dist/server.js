549f7036537c2ddaefb8875dd3e5990b9b50033b
Cyril Prue <kir8mir>
2022-11-03 10:39:00 +0200

    Heroku

--- server/dist/server.js
+++ server/dist/server.js
@@ -6,9 +6,9 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(cors());
-app.use(express.static('dist/data'));
+app.use(express.static('/data'));
 app.use(express.json());
-const phones = JSON.parse(await readFile(new URL('dist/data/api/phones.json', import.meta.url), 'utf-8'));
+const phones = JSON.parse(await readFile(new URL('/data/api/phones.json', import.meta.url), 'utf-8'));
 app.get('/phones', (req, res) => {
     console.log('Server is running');
     res.setHeader('Access-Control-Allow-Origin', '*');
