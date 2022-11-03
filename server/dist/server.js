5bc98d357e37582822f16a3e49be800b3c29319e
Cyril Prue <kir8mir>
2022-11-03 19:34:25 +0200

    heroku

--- server/dist/server.js
+++ server/dist/server.js
@@ -3,10 +3,17 @@ import express from 'express';
 // import phonesFromServer from '../data/api/phones.json'
 import { readFile } from 'fs/promises';
 import cors from 'cors';
+import path from 'path';
+import { dirname } from 'path';
+
+
+const __dirname = path.resolve();
+
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(cors());
-app.use(express.static('dist/dist/data'));
+console.log('123',__dirname)
+app.use(express.static(path.join(__dirname, 'dist/dist/data')));
 app.use(express.json());
 const phones = JSON.parse(await readFile(new URL('dist/data/api/phones.json', import.meta.url), 'utf-8'));
 app.get('/phones', (req, res) => {
