96ca201048d5b8b7a94512b98243ce861c04a5b4
Cyril Prue <kir8mir>
2022-11-03 19:13:34 +0200

    Heroku

--- server/dist/server.js
+++ server/dist/server.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(cors());
-app.use(express.static('data'));
+app.use(express.static('dist/dist/data'));
 app.use(express.json());
 const phones = JSON.parse(await readFile(new URL('dist/data/api/phones.json', import.meta.url), 'utf-8'));
 app.get('/phones', (req, res) => {
