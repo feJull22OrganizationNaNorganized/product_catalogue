eb5bf07e06532da3d8ad1146a5351e50a1b2d56b
Oksana Kyryienko <kseniyals1609@gmail.com>
2022-11-03 12:39:52 +0200

    names for header`s className were changed

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,79 +1,79 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
 import { BurgerMenu } from '../BurgerMenu/BurgerMenu';
-import './Header.scss';
+import s from './Header.module.scss';
 
 export function Header() {
   return (
     <>
-      <div className="header" id="header">
-        <div className="header__container">
-          <div className="menu__container">
-            <div className="images">
-              <div className="nice_gadgets">
+      <div className={s.header} id="header">
+        <div className={s.header__container}>
+          <div className={s.menu__container}>
+            <div className={s.images}>
+              <div className={s.nice_gadgets}>
                 <img
-                  className="nice"
+                  className={s.nice}
                   src={require('../../images/Nice_Gadgets.png')}
                   alt="footerLogo"
                 />
               </div>
-              <div className="img_hand">
+              <div className={s.img_hand}>
                 <img
-                  className="hand"
+                  className={s.hand}
                   src={require('../../images/imageHand.png')}
                   alt="hand_ok"
                 />
               </div>
             </div>
-            <nav className="header__menu">
-              <div className="active">
-                <ul className="nav__list">
-                  <li className="nav__item">
+            <nav className={s.header__menu}>
+              <div className={s.active}>
+                <ul className={s.nav__list}>
+                  <li className={s.nav__item}>
                     <a className="nav__link is-active" href="#section-1">
                       Home
                     </a>
                   </li>
                 </ul>
-                <div className="line"></div>
+                <div className={s.line}></div>
               </div>
-              <ul className="nav__list">
-                <li className="nav__item">
-                  <a className="nav__link" href="#section-2">
+              <ul className={s.nav__list}>
+                <li className={s.nav__item}>
+                  <a className={s.nav__link} href="#section-2">
                     Phones
                   </a>
                 </li>
               </ul>
-              <ul className="nav__list">
-                <li className="nav__item">
-                  <a className="nav__link" href="#section-3">
+              <ul className={s.nav__list}>
+                <li className={s.nav__item}>
+                  <a className={s.nav__link} href="#section-3">
                     Tablets
                   </a>
                 </li>
               </ul>
-              <ul className="nav__list">
-                <li className="nav__item">
-                  <a className="nav__link" href="#section-4">
+              <ul className={s.nav__list}>
+                <li className={s.nav__item}>
+                  <a className={s.nav__link} href="#section-4">
                     Accessories
                   </a>
                 </li>
               </ul>
             </nav>
           </div>
-          <div className="header__top">
-            <div className="top_img">
+          <div className={s.header__top}>
+            <div className={s.top_img}>
               <a href="#header">
-                <div className="container_img">
+                <div className={s.container_img}>
                   <img
-                    className="heart"
+                    className={s.heart}
                     src={require('../../images/heart.png')}
                     alt="heart"
                   />
                 </div>
               </a>
               <a href="">
-                <div className="container_img">
+                <div className={s.container_img}>
                   <img
-                    className="bucket"
+                    className={s.bucket}
                     src={require('../../images/bucket.png')}
                     alt="bucket"
                   />
@@ -81,9 +81,9 @@ export function Header() {
               </a>
             </div>
             <a href="#menu">
-              <div className="container_img">
+              <div className={s.container_img}>
                 <img
-                  className="cart"
+                  className={s.cart}
                   src={require('../../images/cart.png')}
                   alt="Сart"
                 />
