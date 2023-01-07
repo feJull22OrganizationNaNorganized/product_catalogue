75dfa45b7ee3dcf7db59c0fcab94a424423a4d52
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-01-07 11:52:59 +0200

    first burger was added

--- client/src/components/BurgerMenu/BurgerMenu.tsx
+++ client/src/components/BurgerMenu/BurgerMenu.tsx
@@ -11,13 +11,41 @@ export const BurgerMenu: React.FC<Props> = ({ handleChangeOpen }) => {
   return (
     <div className='hi'>
       <div className='for-burger'>
+        <img src="../../images/NiceGadgets.png" alt="nice" />
         <img
           src={require('../../images/Close.png')}
           alt="close"
           onClick={() => handleChangeOpen()}
         />
       </div>
-      <Link to='/'>aa</Link>
+      <div className='burger-list'>
+        <Link 
+          to='/' 
+          className='burger_item'
+          onClick={() => handleChangeOpen()}
+        >Home</Link>
+        <Link 
+          to='/product_catalogue' 
+          className='burger_item'
+          onClick={() => handleChangeOpen()}
+        >Phones</Link>
+        <Link 
+          to='/tablets ' 
+          className='burger_item'
+          onClick={() => handleChangeOpen()}
+        >tablets</Link>
+        <Link 
+          to='/Accessories' 
+          className='burger_item'
+          onClick={() => handleChangeOpen()}
+        >accessories</Link>
+      </div>
+
+      <div className=' bottom-burger'>
+        <img className='bootem-item' src="../../images/heart.png" alt="heart" />
+        <img  src="../../images/backet.png" alt="backet" />
+
+      </div>
     </div>
   );
 };
